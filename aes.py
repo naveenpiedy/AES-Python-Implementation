748063ba1d0aac7bb7e164a25b161eeaed39128c
naveenpiedy <nav.bugs.thepoet@gmail.com>
2017-11-14 12:33:54 -0700

    Key Gen

---
+++ aes.py
@@ -0,0 +1,14 @@
+import secrets
+
+def genKey(k):
+    randomKey = []
+    for i in range(0, k):
+        temp = secrets.randbits(8)
+        randomKey.append(temp)
+    print(sorted(randomKey), len(randomKey))
+
+def main():
+    genKey(16)
+    #print(x)
+if __name__ == '__main__':
+    main()
